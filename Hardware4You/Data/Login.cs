765f2749269bd4796a3c519238e54f895a416910
janah <janah@LAPTOP-T2BI9OU8>
2023-12-12 23:04:52 +0100

    minor changes: implemented fontawesome styles for icons, added some fields for products, started login page

---
+++ Hardware4You/Data/Login.cs
@@ -0,0 +1,9 @@
+﻿namespace Hardware4You.Data
+{
+    public class Login
+    {
+        public string? Email { get; set; }
+
+        public string? Password { get; set; }
+    }
+}
--- Hardware4You/Data/Product.cs
+++ Hardware4You/Data/Product.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Syncfusion.ExcelExport;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +10,16 @@ namespace Hardware4You.Data
     public partial class Product
     {
         public long Id { get; set; }
+
         public string Name { get; set; } = null!;
+
         public int Price { get; set; }
+
+        public string Description { get; set; } = null!;
+        
+        //TODO: how to set image for each product differently db
+        //public Image Image { get; set; };
+
+        public string Specifications { get; set; } = null!;
     }
 }
