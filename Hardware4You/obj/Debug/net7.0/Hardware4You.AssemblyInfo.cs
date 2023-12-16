251aab9e25809b0cf4b2de43a0dae8721b90ea95
janah <janah@LAPTOP-T2BI9OU8>
2023-12-16 18:55:11 +0100

    changed main and nav menu authorisation

--- Hardware4You/obj/Debug/net7.0/Hardware4You.AssemblyInfo.cs
+++ Hardware4You/obj/Debug/net7.0/Hardware4You.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hardware4You")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+599d619c9e286b740bb973d2810f3ddf0ac8bb43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b911403d6409306c7e40d04f6cb503390bde69c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hardware4You")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hardware4You")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
